6d6b1d68a90dead45c16de6758c68bdf5ae393fa
AsadbekSWE <asadbekgulomov2010@gmail.com>
2025-06-03 17:14:54 +0500

    code finished

--- src/MainProject.jsx
+++ src/MainProject.jsx
@@ -1,34 +1,33 @@
 import React from "react";
 
-
+const Wrapper = styled.div`
+  width: 100%;
+  margin: 0;
+  padding: 0;
+`;
 
 //pages
 import Landing from "./pages/Landing";
 import Content from "./pages/Content";
 import Features from "./pages/Features";
 import Form from "./pages/Form";
-// import Testimonial from "./pages/Testimonial";
+import Testimonial from "./pages/Testimonial";
+import styled from "styled-components";
 // import CallToAction from "./pages/CallToAction";
-import ShowCase from "./pages/ShowCase";
+// import ShowCase from "./pages/ShowCase";
 // import Team from "./pages/Team";
 // import Contact from "./pages/Contact";
 // import PricingTable from "./pages/PricingTable";
 // import Footer from "./pages/Footer";
 
-
 export default function MainProject() {
-    return (
-        <>
-        <Landing />
-        <Content />
-        <Features />
-        <Form />
-
-
-        <ShowCase />
-
-
-        
-        </>
-    )
+  return (
+    <Wrapper>
+      <Landing />
+      <Content />
+      <Features />
+      <Form />
+      <Testimonial />
+    </Wrapper>
+  );
 }
